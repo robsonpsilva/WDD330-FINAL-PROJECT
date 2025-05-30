86f86d74a74a3461f246cbc978458562a8bbb929
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-05-29 22:16:03 -0300

    Versão com o join funcionando

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
+        cart: resolve(__dirname, "src/details/details.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         product_list: resolve(__dirname, "src/product_listing/index.html"),
