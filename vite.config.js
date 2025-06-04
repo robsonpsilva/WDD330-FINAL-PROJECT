3f10d8bb9925c96c2bbb0e47edcb1e83504d3ccd
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-06-04 13:42:45 -0300

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,7 @@ export default defineConfig({
         details: resolve(__dirname, "src/details/details.html"),
         join: resolve(__dirname, "src/join/join.html"),
         sell: resolve(__dirname, "src/cart/sell.html"),
-        schedule: resolve(__dirname, "src/schedule/schedule.html"),
-        scripts: resolve(__dirname,"src/js/base.js")
+        schedule: resolve(__dirname, "src/schedule/schedule.html")
       },
     },
   },
