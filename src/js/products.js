a74093c933e7f38117f67243d59a3ec6c1c99b29
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-06-09 12:53:57 -0300

    modified:   src/js/products.js

--- src/js/products.js
+++ src/js/products.js
@@ -11,20 +11,20 @@ export async function fetchProducts() {
   }
 }
 
-export function displayProducts() {
-  const productList = document.getElementById("product-list");
-  products.forEach(product => {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-      <img src="${product.image}" alt="${product.name}" loading="lazy">
-      <h2>${product.name}</h2>
-      <p>Price: $${product.price.toFixed(2)}</p>
-      <label for="quantity-${product.id}">Quantity:</label>
-      <input type="number" id="quantity-${product.id}" min="1" value="1">
-      <br><br>
-      <button onclick="addToCart(${product.id})">Add to Cart</button>
-    `;
-    productList.appendChild(card);
-  });
-}
+  export function displayProducts() {
+    const productList = document.getElementById("product-list");
+    products.forEach(product => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.name}" loading="lazy">
+        <h2>${product.name}</h2>
+        <p>Price: $${product.price.toFixed(2)}</p>
+        <label for="quantity-${product.id}">Quantity:</label>
+        <input type="number" id="quantity-${product.id}" min="1" value="1">
+        <br><br>
+        <button onclick="addToCart(${product.id})">Add to Cart</button>
+      `;
+      productList.appendChild(card);
+    });
+  }
